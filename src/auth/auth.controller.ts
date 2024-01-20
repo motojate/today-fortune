d772a3baa49a2d5b6b25e2077be8a90e029d8f8f
motojate <motojate@naver.com>
2024-01-21 02:59:54 +0900

    [feat] today-fortune - import 경로명 변경 - app 모듈 import 주입

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, HttpCode, Post, Response } from '@nestjs/common';
 import { Response as ExpressResponse } from 'express';
-import { LoginAuthDto } from './dtos/auth.dto';
-import { AuthService } from './auth.service';
+import { LoginAuthDto } from 'src/auth/dtos/auth.dto';
+import { AuthService } from 'src/auth/auth.service';
+import { ACCESS_TOKEN_KEY } from 'src/shared/constants/data';
 
 @Controller('auth')
 export class AuthController {
@@ -11,7 +12,7 @@ export class AuthController {
   @HttpCode(201)
   async login(@Response() res: ExpressResponse, @Body() dto: LoginAuthDto) {
     const tokens = await this.authService.login(dto);
-    res.cookie('access_token', tokens.accessToken, { httpOnly: true });
+    res.cookie(ACCESS_TOKEN_KEY, tokens.accessToken, { httpOnly: true });
     res.send();
   }
 }
--- src/fortune/fortune.module.ts
+++ src/fortune/fortune.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { FortuneService } from './fortune.service';
+import { FortuneService } from 'src/fortune/fortune.service';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
-import { FortuneController } from './fortune.controller';
+import { FortuneController } from 'src/fortune/fortune.controller';
 import { CodeService } from 'src/code/code.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [FortuneService, PrismaService, CodeService],
+  providers: [FortuneService, PrismaService, CodeService, JwtService],
   controllers: [FortuneController],
 })
 export class FortuneModule {}
